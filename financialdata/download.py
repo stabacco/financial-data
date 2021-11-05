a07e96e9ebf75a37175769906838642f1ee1bc5a
Stefano Tabacco <tabacco.stefano@gmail.com>
2021-11-05 17:00:11 +1100

    Writing file

--- financialdata/download.py
+++ financialdata/download.py
@@ -1,4 +1,8 @@
+import os
+
 import pandas_ta as ta
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def download_ticker(ticker_name):
@@ -8,6 +12,7 @@ def download_ticker(ticker_name):
 
     df = yf.download(ticker_name, period="5y", interval="1d", auto_adjust=True)
     df.ta.strategy(ta.AllStrategy)
+    df.to_csv(os.path.join(dir_path, "../output", f"{ticker_name}.csv"))
     return df
 
 
