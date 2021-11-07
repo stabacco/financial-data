78c0e46de8ac345319fb77351a6b554db6beb0ee
Stefano Tabacco <tabacco.stefano@gmail.com>
2021-11-08 09:43:50 +1100

    Updated strategy

--- financialdata/download.py
+++ financialdata/download.py
@@ -15,13 +15,35 @@ def download_ticker(ticker_name):
 
 def add_indicators(df: pd.DataFrame) -> pd.DataFrame:
     """Add technical indicators to the dataframe."""
-    df.ta.strategy(ta.AllStrategy)
+    strategy = ta.Strategy(
+        name="Common Price and Volume SMAs",
+        description="Common Price SMAs: 10, 20, 50, 200 and Volume SMA: 20.",
+        ta=[
+            {"kind": "sma", "length": 10},
+            {"kind": "sma", "length": 20},
+            {"kind": "sma", "length": 50},
+            {"kind": "sma", "length": 150},
+            {"kind": "sma", "length": 200},
+            {"kind": "bbands", "length": 20},
+            {"kind": "rsi"},
+            {"kind": "stoch"},
+            {
+                "kind": "kc",
+            },
+            {"kind": "macd"},
+            {"kind": "roc"},
+            {"kind": "atr", "length": 14},
+            {"kind": "stddev"},
+        ],
+    )
+    df.ta.strategy(strategy)
     return df
 
 
 def save_file(df: pd.DataFrame, ticker_name: str) -> str:
     """Save the dataframe to a csv file."""
-    # df.to_csv(os.path.join(dir_path, "../output", f"{ticker_name}.csv.gz"), compression='gzip')
+    # df.to_csv(os.path.join(dir_path, "../output",
+    # f"{ticker_name}.csv.gz"), compression='gzip')
     fname = os.path.join(dir_path, "../output", f"{ticker_name}.parquet")
     df.to_parquet(
         fname,
@@ -30,7 +52,7 @@ def save_file(df: pd.DataFrame, ticker_name: str) -> str:
     return df
 
 
-def process_tickers(*ticker_names: list) -> list:
+def process_tickers(*ticker_names: str) -> list:
     """Download the ticker data from the web."""
 
     df_list = []
@@ -39,7 +61,7 @@ def process_tickers(*ticker_names: list) -> list:
             print("Processing ", ticker_name)
             df = download_ticker(ticker_name)
             df = add_indicators(df)
-            df_list.append(save_file(df, ticker_name))
+            df_list.append(save_file(df=df, ticker_name=ticker_name))
         except Exception as e:
             print("failed to process: reason {e}".format(e=e))
     return df_list
